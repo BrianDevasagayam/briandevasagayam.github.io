be1601265db17f5955276ad36a2cbcd7bbcbda6a
Brian Devasagayam <brian_ad@outlook.com>
2018-07-02 11:35:07 -0400

    Add Home component and add apath to router

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <p>{{ welcome }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    data() {
+        return {
+            welcome : 'Welcome to the Home page'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
