84037199a7ad6e7e118eef405ed706db32794673
Brian Devasagayam <brian_ad@outlook.com>
2018-07-02 12:44:32 -0400

    Add links in navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -4,7 +4,33 @@
 
 <script>
 export default {
-
+    name : 'Navigation',
+    data() {
+        return {
+            links: [
+                {
+                    id: 0,
+                    text: 'Home',
+                    path: '/'
+                },
+                {
+                    id: 1,
+                    text: 'Experience',
+                    path: '/experience'
+                },
+                {
+                    id: 2,
+                    text: 'Projects',
+                    path: '/projects'
+                },
+                {
+                    id: 3,
+                    text: 'Resume',
+                    path: '/resume'
+                }
+            ]
+        }
+    }
 }
 </script>
 
