392d4e7e0e7db8a149eae9f826013f0d13cfcc34
Brian Devasagayam <brian_ad@outlook.com>
2018-07-07 17:46:52 -0400

    Add list of nav items

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,13 +1,21 @@
 <template>
-
+    <div id="navigation">
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Experience</li>
+                <li>Projects</li>
+                 <li>Resume</li>
+            </ul>
+        </nav> 
+    </div>
 </template>
 
 <script>
 export default {
-    name : 'Navigation',
-}
+  name: "Navigation"
+};
 </script>
 
 <style>
-
 </style>
